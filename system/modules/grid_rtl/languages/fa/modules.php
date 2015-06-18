42dcd79c4e8bc80e48528cfee1dd2ee6abfaf66f
hamidabbaszadeh <abbaszadeh.h@gmail.com>
2015-06-18 09:49:41 +0430

    Adding persian language

---
+++ system/modules/grid_rtl/languages/fa/modules.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/contao-grid-rtl/language/fa/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2015-06-18T09:46:58+04:30
+ */
+
+
+$GLOBALS['TL_LANG']['MOD']['grid_rtl']['0'] = 'شبکه‌بندی راست به چپ کانتائو';
+$GLOBALS['TL_LANG']['MOD']['grid_rtl']['1'] = 'شبکه‌بندی راست به چپ کانتائو با ۱۲ ستون';
+
---
+++ system/modules/grid_rtl/languages/fa/tl_layout.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/contao-grid-rtl/language/fa/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2015-06-18T09:46:58+04:30
+ */
+
+
+$GLOBALS['TL_LANG']['tl_layout']['grid_rtl']['css']['0'] = 'شبکه‌بندی ۱۲ ستونی (راست به چپ)';
+$GLOBALS['TL_LANG']['tl_layout']['grid_rtl']['css']['1'] = 'شبکه‌بندی ۱۲ ستونی واکنش‌گرا راست به چپ که با کلاس‌های "grid1" تا "grid12" و "offset1" تا "offset12" مشخص شده را اضافه می‌کند.';
+
